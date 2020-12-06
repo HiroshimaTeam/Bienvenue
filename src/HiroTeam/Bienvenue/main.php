fc9355c0ea662789964009d45cc472d869258672
HiroTeam <65712904+HiroshimaTeam@users.noreply.github.com>
2020-12-06 13:16:45 +0100

    Update main.php

--- src/HiroTeam/Bienvenue/main.php
+++ src/HiroTeam/Bienvenue/main.php
@@ -17,7 +17,7 @@ class main extends PluginBase{
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
-        $commandName = $command->getName();
+        $commandName = $command->getName(); //Récuperation dans une variable du nom de la commande
         if($sender instanceof Player){ //si sender= l'envoyeur de la commande est un Player psq la console ne peut pas avoir de perm
             if(strtolower($commandName) === 'bienvenue') { //strtolower = mettre la commande en minuscule
                 $sender->sendMessage('Bienvenue à toi sur cette magnifique vidéo !');
